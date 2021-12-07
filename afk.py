ef0140a4ed08fba3ad9ef2bf227585676df0a726
SuperLego9000 <82184903+SuperLego9000@users.noreply.github.com>
2021-12-07 07:13:05 -0800

    hotfix
    changed some stuff for preformance

--- afk.py
+++ afk.py
@@ -1,6 +1,15 @@
 from rbxmove import rbxmove as rbx
 from time import sleep as wait
+import random as r
+from win32gui import GetWindowText, GetForegroundWindow
 print("tab to roblox")
 while True:
-    rbx.countto(5)
-    rbx.game.reset(delay=.2)
+    if GetWindowText(GetForegroundWindow())=="Roblox":
+        rbx.move(r.choice(['w','a','s','d']),r.randint(5, 9))#move asap so we lower afk chance
+        print("afk-kick timer restarted")
+        rbx.countto(r.randint(3*60,5*60))#wait 3-5 minutes
+        if GetWindowText(GetForegroundWindow())=="Roblox":#if we still in roblox
+            rbx.move(r.choice(['w','a','s','d']),r.randint(5, 9))#move some
+            rbx.game.reset()#try to reset
+    else:
+        print("!~!~!  TAB TO ROBLOX !~!~!")
