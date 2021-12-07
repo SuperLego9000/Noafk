e75a290e535ef41eeb72c05966192c924f8b7785
SuperLego9000 <82184903+SuperLego9000@users.noreply.github.com>
2021-12-07 06:34:54 -0800

    Full afk support
    now you can choose to fully AFK on launch.

--- Noafk.py
+++ Noafk.py
@@ -1,8 +1,3 @@
-"""
-wait 15
-alert by making noise
-allow user to stop the noise or/and restart the timer
-"""
 import pyautogui as gui
 import time
 wait=time.sleep
@@ -10,7 +5,7 @@ wait=time.sleep
 from playsound import playsound
 import threading
 
-what=gui.confirm("Method","Noafk","Alarm","Full afk")
+what=gui.confirm("Method","Noafk",["Alarm","Full afk"])
 
 if what=="Alarm":
     import alarm
